44a19b3df6bbfea6f7bde88d95f35cf237b40122
Aaron DeLong <aarondelong93@gmail.com>
2018-06-18 12:17:18 -0400

    Completed Project

--- app/src/main/java/com/example/aarondelong/guessinggame/GameActivity.java
+++ app/src/main/java/com/example/aarondelong/guessinggame/GameActivity.java
@@ -3,7 +3,6 @@ package com.example.aarondelong.guessinggame;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,9 +17,9 @@ public class GameActivity extends AppCompatActivity {
 
     private int generateNumber;
     private int numberOfGuesses = 0;
-    private final int MAX_GUESS_COUNT = 4;
     public static final String winningNumber = "WINNING_NUMBER";
 
+//    TODO fix restarting game!!!
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,12 +29,22 @@ public class GameActivity extends AppCompatActivity {
         clue = findViewById(R.id.clue_textview);
         guess = findViewById(R.id.guess_edittext);
 
-//        This generates a random number between 1 and 100
-        generateNumber = (int) Math.ceil(Math.random() * 100);
 
 //        Toast.makeText(this, Integer.toString(generateNumber), Toast.LENGTH_SHORT).show();
 
         setListener();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //This generates a random number between 1 and 100
+        generateNumber = (int) Math.ceil(Math.random() * 100);
+
+        numberOfGuesses = 0;
+        clue.setVisibility(View.INVISIBLE);
+        guess.setText("");
     }
 
     private void setListener() {
@@ -50,60 +59,66 @@ public class GameActivity extends AppCompatActivity {
     //Check to make sure that the user has put in a valid number
     private void validateGuess() {
 
-        int userGuess = Integer.parseInt(guess.getText().toString());
-        if (userGuess > 100 || userGuess <= 0)
-            clue.setText("Enter a number between 1 and 100");
-        clue.setVisibility(View.VISIBLE);
-        guess.setText("");
-    } else
-        {checkGuess(userGuess);
-    }
+        try {
 
-    catch(
-            NumberFormatException nfe)
-            {
-            clue.setText("Enter a number");
-            clue.setVisibility(View.VISIBLE);
+            int userGuess = Integer.parseInt(guess.getText().toString());
+            if (userGuess > 100 || userGuess <= 0) {
+                clue.setText(R.string.choose_number);
+                clue.setVisibility(View.VISIBLE);
+                guess.setText("");
+            } else {
+
+                checkGuess(userGuess);
             }
 
-//This method will take the input and check it against the generateNumber. Depending on the outcome it will change the view accordingly or take us to the ResultsActivity;
-private void checkGuess(int userGuess){
+        } catch (NumberFormatException nfe) {
+            clue.setText(R.string.enter_number);
+            clue.setVisibility(View.VISIBLE);
+        }
+    }
 
-        if(userGuess==generateNumber){
+    //This method will take the input and check it against the generateNumber. Depending on the outcome it will change the view accordingly or take us to the ResultsActivity;
+    private void checkGuess(int userGuess) {
+
+        int MAX_GUESS_COUNT = 4;
+        if (userGuess == generateNumber) {
 //            Goes to ResultsActivity. User has guessed correctly
-        Intent winner=new Intent(this,ResultsActivity.class);
-        startActivity(winner);
+            Intent winner = new Intent(this, ResultsActivity.class);
+            startActivity(winner);
 
-        else if(numberOfGuesses==MAX_GUESS_COUNT){
+        } else if (numberOfGuesses == MAX_GUESS_COUNT) {
 //                Goes to ResultsActivity. User has ran out of chances
-        Intent loser=new Intent(this,ResultsActivity.class);
-        loser.putExtra(winningNumber,generateNumber);
-        startActivity(loser);
+                Intent loser = new Intent(this, ResultsActivity.class);
+                loser.putExtra(winningNumber, generateNumber);
+                startActivity(loser);
 
-        }else if(userGuess<generateNumber){
-//                TODO Update clue TextView to say higher. Set visibility to VISIBLE, set guess EditText to "" and increment numberOfGuesses by 1
+            } else if (userGuess < generateNumber) {
+//                Update clue TextView to say higher. Set visibility to VISIBLE, set guess EditText to "" and increment numberOfGuesses by 1
 
-        clue.setText(R.string.higher);
-        clue.setVisibility(View.VISIBLE);
-        guess.setText("");
-        numberOfGuesses++;
+                clue.setText(R.string.higher);
+                clue.setVisibility(View.VISIBLE);
+                guess.setText("");
+                numberOfGuesses++;
+                Toast.makeText(this, getString(R.string.chances_left, (5 - numberOfGuesses)), Toast.LENGTH_SHORT).show();
 
-        }else if(userGuess>generateNumber){
-        // TODO Update clue TextView to say lower. Set visibility to VISIBLE, set guess EditText to "" and increment numberOfGuesses by 1
 
-        clue.setText(R.string.lower);
-        clue.setVisibility(View.VISIBLE);
-        guess.setText("");
-        numberOfGuesses--;
+            } else if (userGuess > generateNumber) {
+                //Update clue TextView to say lower. Set visibility to VISIBLE, set guess EditText to "" and increment numberOfGuesses by 1
 
+                clue.setText(R.string.lower);
+                clue.setVisibility(View.VISIBLE);
+                guess.setText("");
+                numberOfGuesses++;
+            Toast.makeText(this, getString(R.string.chances_left, (5 - numberOfGuesses)), Toast.LENGTH_SHORT).show();
+
+            }
         }
-        }
 
-        }
 
-@Override
-public void onBackPressed(){
+
+    @Override
+    public void onBackPressed() {
 //    Removed super.onBackPressed to make sure that if the back button is pressed nothing will happen.
-        }
-        }
+    }
+}
 
--- app/src/main/java/com/example/aarondelong/guessinggame/ResultsActivity.java
+++ app/src/main/java/com/example/aarondelong/guessinggame/ResultsActivity.java
@@ -1,15 +1,60 @@
 package com.example.aarondelong.guessinggame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import static com.example.aarondelong.guessinggame.GameActivity.winningNumber;
 
 public class ResultsActivity extends AppCompatActivity {
+
+    private Button playAgainButton;
+    private TextView correctNumberTextview;
+    private TextView resultsTextview;
+    private ImageView resultsImageview;
+    private Intent intent;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
 
-//        TODO get winning number from intent IF user has lost
+        playAgainButton = findViewById(R.id.restart_button);
+        correctNumberTextview = findViewById(R.id.number_textview);
+        resultsTextview = findViewById(R.id.results_textview);
+        resultsImageview = findViewById(R.id.winning_imageview);
+
+        setListener();
+
+//      Get winning number from intent IF user has lost
+        intent = getIntent();
+        if(intent.hasExtra(winningNumber)) {
+            setLosingData();
+        }
+    }
+
+    private void setLosingData() {
+        int winningNumber = intent.getIntExtra(GameActivity.winningNumber, 0);
+        resultsTextview.setText(R.string.losing_message);
+        correctNumberTextview.setText(getString(R.string.winning_number, winningNumber));
+        correctNumberTextview.setVisibility(View.VISIBLE);
+
+        resultsImageview.setImageResource(R.drawable.losingsadface);
+    }
+
+    private void setListener() {
+        playAgainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+
+            }
+        });
     }
 }
